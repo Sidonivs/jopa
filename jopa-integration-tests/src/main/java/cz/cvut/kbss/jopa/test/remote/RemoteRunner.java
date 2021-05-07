691a0336280e4d5d2becc2fc6573ba0a20630ff5
Zdeněk <kotrlzde@fel.cvut.cz>
2021-05-07 15:11:16 +0200

    finished tests for evaluation

--- jopa-integration-tests/src/main/java/cz/cvut/kbss/jopa/test/remote/RemoteRunner.java
+++ jopa-integration-tests/src/main/java/cz/cvut/kbss/jopa/test/remote/RemoteRunner.java
@@ -2,6 +2,7 @@ package cz.cvut.kbss.jopa.test.remote;
 
 import cz.cvut.kbss.jopa.model.EntityManager;
 import cz.cvut.kbss.jopa.test.remote.model.Term;
+import cz.cvut.kbss.jopa.test.remote.model.TermInfo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,12 +14,41 @@ public class RemoteRunner {
     }
 
     @Test
-    public void loadLocalityTermTest() {
+    public void localityTermTest() {
         EntityManager em = PersistenceFactory.createEntityManager();
+
         final Term localityTerm = em.find(Term.class, "http://onto.fel.cvut.cz/ontologies/slovnik/ml-test/pojem/lokalita");
-        System.out.println("LOCALITY TERM URI: " + localityTerm.getUri());
-        System.out.println("LOCALITY TERM DEFINITION: " + localityTerm.getDefinition());
-        System.out.println("LOCALITY TERM DESCRIPTION: " + localityTerm.getDescription());
-        System.out.println(localityTerm.getRoot());
+
+        assertEquals(localityTerm.getUri().toString(), "http://onto.fel.cvut.cz/ontologies/slovnik/ml-test/pojem/lokalita");
+        assertEquals(localityTerm.getChildren().size(), 1);
+        for (TermInfo termInfo : localityTerm.getChildren()) {
+            assertEquals(termInfo.getUri().toString(), "http://onto.fel.cvut.cz/ontologies/slovnik/ml-test/pojem/stabilizovana-chranena-lokalita");
+        }
+        assertEquals(localityTerm.getRoot(), true);
+    }
+
+    @Test
+    public void documentationTermTest() {
+        EntityManager em = PersistenceFactory.createEntityManager();
+
+        final Term term = em.find(Term.class, "http://onto.fel.cvut.cz/ontologies/slovnik/ml-test/pojem/dokumentace");
+
+        assertEquals(term.getUri().toString(), "http://onto.fel.cvut.cz/ontologies/slovnik/ml-test/pojem/dokumentace");
+        assertEquals(term.getChildren().size(), 1);
+        for (TermInfo termInfo : term.getChildren()) {
+            assertEquals(termInfo.getUri().toString(), "http://onto.fel.cvut.cz/ontologies/slovnik/ml-test/pojem/term-six");
+        }
+        assertEquals(term.getRoot(), true);
+    }
+
+    @Test
+    public void stabilizedProtectedLocalityTermTest() {
+        EntityManager em = PersistenceFactory.createEntityManager();
+
+        final Term term = em.find(Term.class, "http://onto.fel.cvut.cz/ontologies/slovnik/ml-test/pojem/stabilizovana-chranena-lokalita");
+
+        assertEquals(term.getUri().toString(), "http://onto.fel.cvut.cz/ontologies/slovnik/ml-test/pojem/stabilizovana-chranena-lokalita");
+        assertNull(term.getChildren());
+        assertEquals(term.getRoot(), false);
     }
 }
